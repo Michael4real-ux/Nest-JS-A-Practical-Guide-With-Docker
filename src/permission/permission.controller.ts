7ac4e26e29aae17db3c1cb8995b30ea433457630
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2023-01-25 10:07:51 +0100

    Custom decorator added

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { HasPermission } from './has-permission.decorator';
 import { PermissionService } from './permission.service';
 
 @Controller('permissions')
 export class PermissionController {
   constructor(private permissionService: PermissionService) {}
   @Get()
+  @HasPermission('view_permissions')
   async all() {
     return this.permissionService.all();
   }
